b492fb68a0198c4d22704edb68a0628d65c9dcb8
Nicklas W <Nicklas2002@hotmail.com>
2023-10-24 11:29:59 +0200

    UserMapper Done

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -2,7 +2,55 @@ package app.entities;
 
 public class User {
 
-    public User(){
+    private int userID;
+    private String email;
+    private String password;
+    private int balance;
+    private boolean admin;
 
+    /**
+     *
+     * @param userID
+     * @param email
+     * @param password
+     * @param balance
+     * @param admin
+     */
+
+    public User(int userID, String email, String password, int balance, boolean admin) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.admin = admin;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                ", admin=" + admin +
+                '}';
     }
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,18 +1,53 @@
 package app.persistence;
 
 import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserMapper {
 
 
-    public static User login(String username, String password, ConnectionPool connectionPool){
-
-        return null;
+    public static User login(String username, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "select * from users where name=? and password=?";
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, username);
+                ps.setString(2, password);
+                ResultSet rs = ps.executeQuery();
+                if(rs.next()){
+                    int id = rs.getInt("id");
+                    int balance = rs.getInt("balanace");
+                    boolean isAdmin = rs.getBoolean("is_admin");
+                    return new User(id,username,password,balance,isAdmin);
+                }else {
+                    throw new DatabaseException("Error while logging in, try again.");
+                }
+            }
+        }catch(SQLException e){
+            throw new DatabaseException("Error while connecting to database");
+        }
     }
 
-    public static User registerUser(String username, String password, ConnectionPool connectionPool){
-        
-        return null;
+    public static void registerUser(String username, String password, int balance, boolean isAdmin, ConnectionPool connectionPool) throws DatabaseException{
+        String sql = "insert into users (email, password, balanace, is_admin) values (?,?,?,?)";
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement(sql)){
+                ps.setString(1,username);
+                ps.setString(2,password);
+                ps.setInt(3,balance);
+                ps.setBoolean(4,isAdmin);
+                int rowsAffected = ps.executeUpdate();
+                if(rowsAffected < 0){
+                    throw new DatabaseException("Error handling registration of user");
+                }
+            }
+        } catch(SQLException e){
+            throw new DatabaseException("Error while connection to database");
+        }
     }
 
 }
