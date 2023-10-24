d14e1c3f45a715163a9b008fc8ab6bf612c8a257
Nicklas W <Nicklas2002@hotmail.com>
2023-10-24 10:40:58 +0200

    EmptyUser & UserController Class

---
+++ src/main/java/app/persistence/UserController.java
@@ -0,0 +1,17 @@
+package app.persistence;
+
+import app.entities.User;
+import app.persistence.ConnectionPool;
+import io.javalin.http.Context;
+
+public class UserController {
+
+    public static User login(Context ctx, ConnectionPool connectionPool){
+        return null;
+    }
+
+    public static User registerUser(Context ctx, ConnectionPool connectionPool){
+        return null;
+    }
+
+}
