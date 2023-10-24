f543395aecd1bcd34fcd09668fdde44d4e75fd7b
Nicklas W <Nicklas2002@hotmail.com>
2023-10-24 10:58:19 +0200

    little update

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,5 +1,18 @@
 package app.persistence;
 
+import app.entities.User;
+
 public class UserMapper {
+
+
+    public static User login(String username, String password, ConnectionPool connectionPool){
+
+        return null;
+    }
+
+    public static User registerUser(String username, String password, ConnectionPool connectionPool){
+        
+        return null;
+    }
 
 }
