43f1de080d659db0b482060d9d6ca98807c2a8b2
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2023-10-26 14:08:16 +0200

    added to BasketController

--- src/main/java/app/controllers/BasketController.java
+++ src/main/java/app/controllers/BasketController.java
@@ -6,7 +6,11 @@ import io.javalin.http.Context;
 public class BasketController {
 
     public static void addToBasket(Context ctx, ConnectionPool connectionPool) {
-        
+        ctx.formParam("top_name");
+        ctx.formParam("top_id");
+        ctx.formParam("bottom_name");
+        ctx.formParam("bottom_name");
+
     }
     public static void loadBasket(Context ctx, ConnectionPool connectionPool) {
 
