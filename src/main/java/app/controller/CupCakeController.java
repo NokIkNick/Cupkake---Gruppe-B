2dd7e8c6e7497090fcc3687318aff0895bfb6f49
chris4567890 <starchristian4@hotmail.com>
2023-10-26 11:35:08 +0200

    Update CupCakeController.java
    updated the update with the update.

--- src/main/java/app/controller/CupCakeController.java
+++ src/main/java/app/controller/CupCakeController.java
@@ -7,20 +7,13 @@ import io.javalin.http.Context;
 
 
 public class CupCakeController {
+    
 
-    public static void getPriceOfObjectTop(Context ctx,ConnectionPool connectionPool){
-
-    }
-
-    public static void getPriceOfObjectBottom(Context ctx, ConnectionPool connectionPool){
+    public static void getTopInfo(Context ctx, ConnectionPool connectionPool){
 
     }
 
-    public static void getNamesTop(Context ctx, ConnectionPool connectionPool){
-
-    }
-
-    public static void getNamesBottom(Context ctx, ConnectionPool connectionPool){
+    public static void getBottomInfo(Context ctx, ConnectionPool connectionPool){
 
     }
 
