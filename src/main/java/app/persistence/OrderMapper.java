dd36626022186ce60f574a1a9e3c2bc428aec1fa
Sir Rolin <nicolaieno@msn.com>
2023-10-30 12:00:21 +0100

    fix after merge

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -39,6 +39,7 @@ public class OrderMapper {
 
             }
         }catch(SQLException e){
+            e.printStackTrace();
             throw new DatabaseException("Error while connecting to database");
         }
     }
